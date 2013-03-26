bb025b9d69f6b1d543990469f50afef738b728fb
Ben Hearsum <bhearsum@mozilla.com>
2013-03-26 15:48:10 -0400

    Add files required for IT to allow Ship It deployments through Chief.

---
+++ scripts/update/update.py
@@ -0,0 +1,67 @@
+"""
+Deploy ship-it.mozilla.org using Chief in dev/stage/production.
+
+Requires commander_ which is installed on the systems that need it.
+
+.. _commander: https://github.com/oremj/commander
+"""
+
+import os
+import sys
+
+sys.path.append(os.path.dirname(os.path.abspath(__file__)))
+
+from commander.deploy import task, hostgroups
+import commander_settings as settings
+
+
+@task
+def update_code(ctx, tag):
+    """Update the code to a specific git reference (tag/sha/etc)."""
+    with ctx.lcd(settings.SRC_DIR):
+        ctx.local('git fetch')
+        ctx.local('git checkout -f %s' % tag)
+
+
+@task
+def checkin_changes(ctx):
+    """Use the local, IT-written deploy script to check in changes."""
+    ctx.local(settings.DEPLOY_SCRIPT)
+
+
+@hostgroups(settings.WEB_HOSTGROUP, remote_kwargs={'ssh_key': settings.SSH_KEY})
+def deploy_app(ctx):
+    """Call the remote update script to push changes to webheads."""
+    ctx.remote(settings.REMOTE_UPDATE_SCRIPT)
+    ctx.remote('/bin/touch %s' % settings.REMOTE_WSGI)
+
+@task
+def update_info(ctx):
+    """Write info about the current state to a publicly visible file."""
+    with ctx.lcd(settings.SRC_DIR):
+        ctx.local('date > media/revision_info.txt')
+        ctx.local('git branch >> media/revision_info.txt')
+        ctx.local('git log -3 >> media/revision_info.txt')
+        ctx.local('git status >> media/revision_info.txt')
+        ctx.local('git rev-parse HEAD > media/revision.txt')
+
+
+@task
+def pre_update(ctx, ref=settings.UPDATE_REF):
+    """Update code to pick up changes to this file."""
+    update_code(ref)
+    
+@task
+def update(ctx):
+    update_info()
+    
+@task
+def deploy(ctx):
+    checkin_changes()
+    deploy_app()
+
+@task
+def update_site(ctx, tag):
+    """Update the app to prep for deployment."""
+    pre_update(tag)
+    update()
