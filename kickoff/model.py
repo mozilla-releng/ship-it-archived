5833b50075ef229aa2382ded24b4f8a143f9908d
Ben Hearsum <bhearsum@mozilla.com>
2014-07-28 10:31:29 -0400

    Backout bug 1032975 because it hit maximum mysql key length.

--- kickoff/model.py
+++ kickoff/model.py
@@ -9,14 +9,11 @@ from mozilla.release.info import getReleaseName
 
 from kickoff import db
 
-
 class Release(object):
-
     """A base class with all of the common columns for any release."""
     name = db.Column(db.String(100), primary_key=True)
     submitter = db.Column(db.String(250), nullable=False)
-    _submittedAt = db.Column('submittedAt', db.DateTime(pytz.utc),
-                             nullable=False, default=datetime.utcnow)
+    _submittedAt = db.Column('submittedAt', db.DateTime(pytz.utc), nullable=False, default=datetime.utcnow)
     version = db.Column(db.String(10), nullable=False)
     buildNumber = db.Column(db.Integer(), nullable=False)
     branch = db.Column(db.String(50), nullable=False)
@@ -27,7 +24,6 @@ class Release(object):
     complete = db.Column(db.Boolean(), nullable=False, default=False)
     status = db.Column(db.String(250), default="")
     mozillaRelbranch = db.Column(db.String(50), default=None, nullable=True)
-    enUSPlatforms = db.Column(db.String(500), default=None, nullable=True)
     comment = db.Column(db.Text, default=None, nullable=True)
 
     # Dates are always returned in UTC time and ISO8601 format to make them
@@ -42,8 +38,7 @@ class Release(object):
 
     def __init__(self, submitter, version, buildNumber, branch,
                  mozillaRevision, l10nChangesets, dashboardCheck,
-                 mozillaRelbranch, enUSPlatforms=None, submittedAt=None,
-                 comment=None):
+                 mozillaRelbranch, submittedAt=None, comment=None):
         self.name = getReleaseName(self.product, version, buildNumber)
         self.submitter = submitter
         self.version = version.strip()
@@ -53,11 +48,11 @@ class Release(object):
         self.l10nChangesets = l10nChangesets
         self.dashboardCheck = dashboardCheck
         self.mozillaRelbranch = mozillaRelbranch
-        self.enUSPlatforms = enUSPlatforms
         if submittedAt:
             self.submittedAt = submittedAt
         if comment:
             self.comment = comment
+
 
     def toDict(self):
         me = {'product': self.product}
@@ -78,9 +73,8 @@ class Release(object):
         self.l10nChangesets = form.l10nChangesets.data
         self.dashboardCheck = form.dashboardCheck.data
         self.mozillaRelbranch = form.mozillaRelbranch.data
-        self.name = getReleaseName(self.product, self.version,
-                                   self.buildNumber)
         self.comment = form.comment.data
+        self.name = getReleaseName(self.product, self.version, self.buildNumber)
 
     @classmethod
     def getRecent(cls, age=timedelta(weeks=7)):
@@ -99,7 +93,6 @@ class Release(object):
     def __repr__(self):
         return '<Release %r>' % self.name
 
-
 class FennecRelease(Release, db.Model):
     __tablename__ = 'fennec_release'
     product = 'fennec'
@@ -111,7 +104,6 @@ class FennecRelease(Release, db.Model):
                    form.mozillaRevision.data, form.l10nChangesets.data,
                    form.dashboardCheck.data, form.mozillaRelbranch.data,
                    form.comment.data)
-
 
 class DesktopRelease(Release):
     partials = db.Column(db.String(100))
@@ -127,7 +119,6 @@ class DesktopRelease(Release):
         self.partials = form.partials.data
         self.promptWaitTime = form.promptWaitTime.data
 
-
 class FirefoxRelease(DesktopRelease, db.Model):
     __tablename__ = 'firefox_release'
     product = 'firefox'
@@ -139,7 +130,6 @@ class FirefoxRelease(DesktopRelease, db.Model):
                    form.mozillaRevision.data, form.l10nChangesets.data,
                    form.dashboardCheck.data, form.mozillaRelbranch.data,
                    form.comment.data)
-
 
 class ThunderbirdRelease(DesktopRelease, db.Model):
     __tablename__ = 'thunderbird_release'
@@ -166,7 +156,6 @@ class ThunderbirdRelease(DesktopRelease, db.Model):
         self.commRevision = form.commRevision.data
         self.commRelbranch = form.commRelbranch.data
 
-
 def getReleaseTable(release):
     """Helper method to figure out what type of release a request is for.
        Because the API methods are not specific to the type of release, we
@@ -180,7 +169,6 @@ def getReleaseTable(release):
         return ThunderbirdRelease
     else:
         raise ValueError("Can't find release table for release %s" % release)
-
 
 def getReleases(ready=None, complete=None):
     filters = {}
@@ -197,40 +185,3 @@ def getReleases(ready=None, complete=None):
             for r in table.query.all():
                 releases.append(r)
     return releases
-
-
-class ReleaseEvents(db.Model):
-
-    """A base class to store release events primarily from buildbot."""
-    __tablename__ = 'release_events'
-    name = db.Column(db.String(100), nullable=False, primary_key=True)
-    sent = db.Column(db.DateTime(pytz.utc), nullable=False)
-    event_name = db.Column(db.String(500), nullable=False, primary_key=True)
-    platform = db.Column(db.String(500), nullable=True)
-    results = db.Column(db.Integer(), nullable=False)
-    chunkNum = db.Column(db.Integer(), default=0, nullable=False)
-    chunkTotal = db.Column(db.Integer(), default=0, nullable=False)
-
-    def __init__(self, name, sent, event_name, platform, results, chunkNum=0,
-                 chunkTotal=0):
-        self.name = name
-        self.sent = sent
-        self.event_name = event_name
-        self.platform = platform
-        self.results = results
-        self.chunkNum = chunkNum
-        self.chunkTotal = chunkTotal
-
-    def toDict(self):
-        for c in self.__table__.columns:
-            me[c.name] = getattr(self, c.name)
-        return me
-
-    @classmethod
-    def createFromRequest(cls, form):
-        return cls(form['name'], form['sent'], form['event_name'],
-                   form['platform'], form['results'], form['chunkNum'],
-                   form['chunkTotal'])
-
-    def __repr__(self):
-        return '<ReleaseEvents %r>' % self.name
