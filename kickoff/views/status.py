f1aa6259652cdb3e856067c582338379adc34410
Rail Aliiev <rail@mozilla.com>
2015-01-05 10:43:56 -0500

    Bug 1116218 - Fix some ship-it errors. r=bhearsum
        * Python 2.6 compatibility fixes
        * PEP8 Fixes
        * getEvents should be called as ReleaseEvents.getEvents

--- kickoff/views/status.py
+++ kickoff/views/status.py
@@ -35,28 +35,30 @@ class StatusAPI(MethodView):
 
         if not form.validate(releaseName):
             errors = form.errors
-            log.error('User Input Failed - {} - ({}, {})'.format(errors.values(),
-                                                                 releaseName,
-                                                                 form.event_name.data))
+            log.error('User Input Failed - %s - (%s, %s)', errors.values(),
+                      releaseName, form.event_name.data)
             cef_event('User Input Failed', CEF_INFO, **errors)
             return Response(status=400, response=errors.values())
 
         # Create a ReleaseEvent object from the request data
         try:
-            releaseEventsUpdate = ReleaseEvents.createFromForm(releaseName, form)
+            releaseEventsUpdate = ReleaseEvents.createFromForm(releaseName,
+                                                               form)
         except Exception as e:
-            log.error('User Input Failed - {} - ({}, {})'.format(e, releaseName,
-                                                                 form.event_name.data))
+            log.error('User Input Failed - %s - (%s, %s)', e, releaseName,
+                      form.event_name.data)
             cef_event('User Input Failed', CEF_ALERT)
             return Response(status=400, response=e)
 
         # Check if this ReleaseEvent already exists in the ReleaseEvents table
-        if db.session.query(ReleaseEvents).\
-                      filter(ReleaseEvents.name==releaseEventsUpdate.name,
-                             ReleaseEvents.event_name==releaseEventsUpdate.event_name).first():
-            msg = 'ReleaseEvents ({}, {}) already exists'.\
-                   format(releaseEventsUpdate.name, releaseEventsUpdate.event_name)
-            log.error('{}'.format(msg))
+        if db.session.query(ReleaseEvents).filter(
+            ReleaseEvents.name == releaseEventsUpdate.name,
+            ReleaseEvents.event_name == releaseEventsUpdate.event_name
+        ).first():
+            msg = 'ReleaseEvents ({r_name}, {e_name}) already exists'.format(
+                r_name=releaseEventsUpdate.name,
+                e_name=releaseEventsUpdate.event_name)
+            log.error(msg)
             cef_event('User Input Failed', CEF_INFO,
                       ReleaseName=releaseEventsUpdate.name)
             return Response(status=400, response=msg)
@@ -64,8 +66,8 @@ class StatusAPI(MethodView):
         # Add a new ReleaseEvents row to the ReleaseEvents table with new data
         db.session.add(releaseEventsUpdate)
         db.session.commit()
-        log.debug('({}, {}) - added to the ReleaseEvents table in the database'.
-                  format(releaseEventsUpdate.name, releaseEventsUpdate.event_name))
+        log.debug('(%s, %s) - added to the ReleaseEvents',
+                  releaseEventsUpdate.name, releaseEventsUpdate.event_name)
 
         return Response(status=200)
 
@@ -76,24 +78,28 @@ class StatusesAPI(MethodView):
         group = request.args.get('group')
         if not group:
             cef_event('User Input Failed', CEF_INFO, group=group)
-            return Response(status=400, response="Got unparseable value for group")
-        events = [(r.name, r.event_name) for r in getEvents(group)]
+            return Response(status=400,
+                            response="Got unparseable value for group")
+        events = [(r.name, r.event_name) for r in
+                  ReleaseEvents.getEvents(group)]
         return jsonify({'events': events})
 
 
 class Status(MethodView):
 
     def get(self, releaseName):
-        status_groups = [('tag', 'Tagging'), ('build', 'Builds'), ('repack', 'Repacks'), 
-                         ('update', 'Update'), ('releasetest', 'Release Test'), 
-                         ('readyforrelease', 'Ready For Release'), 
+        status_groups = [('tag', 'Tagging'), ('build', 'Builds'),
+                         ('repack', 'Repacks'), ('update', 'Update'),
+                         ('releasetest', 'Release Test'),
+                         ('readyforrelease', 'Ready For Release'),
                          ('postrelease', 'Post Release')]
         status = ReleaseEvents.getStatus(releaseName)
         errors = {}
         if not status:
-            errors = ['No release events found for {}'.format(releaseName)]
-        return render_template('status.html', status=ReleaseEvents.getStatus(releaseName), status_groups=status_groups,
-                               errors=errors)
+            errors = ['No release events found for {0}'.format(releaseName)]
+        return render_template(
+            'status.html', status=ReleaseEvents.getStatus(releaseName),
+            status_groups=status_groups, errors=errors)
 
 
 class Statuses(MethodView):
