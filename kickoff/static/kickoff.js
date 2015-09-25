7d7daf927f6998e7cadf65f2dd1c17deab86eee1
Sylvestre Ledru <sylvestre@debian.org>
2015-09-25 12:49:50 +0200

    Automatic coding style provided by jscs

--- kickoff/static/kickoff.js
+++ kickoff/static/kickoff.js
@@ -1,65 +1,65 @@
-function initialSetup(){
-  // initialize tabs and accordion.
-  // Saving last active element using localStorage
-  $( "#tabs" ).tabs({
-    activate: function (event, ui) {
-         localStorage.setItem("active_tab", $( "#tabs" )
-            .tabs("option", "active"));
+function initialSetup() {
+    // initialize tabs and accordion.
+    // Saving last active element using localStorage
+    $('#tabs').tabs({
+        activate: function(event, ui) {
+         localStorage.setItem('active_tab', $('#tabs')
+            .tabs('option', 'active'));
      },
-    active: parseInt(localStorage.getItem("active_tab")),
-  });
+        active: parseInt(localStorage.getItem('active_tab')),
+    });
 
-  $( "#accordion" ).accordion({
-    heightStyle: "content",
-    change: function(event, ui) {
-        localStorage.setItem('active_accordion', $( "#accordion" )
-            .accordion("option", "active"));
+    $('#accordion').accordion({
+        heightStyle: 'content',
+        change: function(event, ui) {
+            localStorage.setItem('active_accordion', $('#accordion')
+                .accordion('option', 'active'));
         },
-    active: parseInt(localStorage.getItem('active_accordion'))
+        active: parseInt(localStorage.getItem('active_accordion'))
     });
 }
 
-function viewReleases(){
-  toLocalDate();
-  // initial sorting by SubmittedAt (descending)
-  // and then saving user table state using localStorage
-  $( "#reviewed" ).dataTable({
-    "bJQueryUI": true,
-    "aaSorting": [[ 3, "desc" ]],
-    "bStateSave": true,
-    "fnStateSave": function (oSettings, oData) {
-        localStorage.setItem( 'DataTables_reviewed'+window.location.pathname, JSON.stringify(oData) );
+function viewReleases() {
+    toLocalDate();
+    // initial sorting by SubmittedAt (descending)
+    // and then saving user table state using localStorage
+    $('#reviewed').dataTable({
+        'bJQueryUI': true,
+        'aaSorting': [[3, 'desc']],
+        'bStateSave': true,
+        'fnStateSave': function(oSettings, oData) {
+        localStorage.setItem('DataTables_reviewed' + window.location.pathname, JSON.stringify(oData));
     },
-    "fnStateLoad": function (oSettings) {
-        return JSON.parse( localStorage.getItem('DataTables_reviewed'+window.location.pathname) );
+        'fnStateLoad': function(oSettings) {
+        return JSON.parse(localStorage.getItem('DataTables_reviewed' + window.location.pathname));
     }
-  });
-  $( "#running" ).dataTable({
-    "bJQueryUI": true,
-    "aaSorting": [[ 2, "desc" ]],
-    "bStateSave": true,
-    "fnStateSave": function (oSettings, oData) {
-        localStorage.setItem( 'DataTables_running'+window.location.pathname, JSON.stringify(oData) );
+    });
+    $('#running').dataTable({
+        'bJQueryUI': true,
+        'aaSorting': [[2, 'desc']],
+        'bStateSave': true,
+        'fnStateSave': function(oSettings, oData) {
+        localStorage.setItem('DataTables_running' + window.location.pathname, JSON.stringify(oData));
     },
-    "fnStateLoad": function (oSettings) {
-        return JSON.parse( localStorage.getItem('DataTables_running'+window.location.pathname) );
+        'fnStateLoad': function(oSettings) {
+        return JSON.parse(localStorage.getItem('DataTables_running' + window.location.pathname));
     }
-  });
+    });
 }
 
 function toLocalDate() {
 
-    $( '.dateDisplay' ).each(function() {
+    $('.dateDisplay').each(function() {
         dateField = $(this).html();
-        if (dateField === "None") {
+        if (dateField === 'None') {
             // Field not set in the db, don't show anything
-            formateddate = "";
+            formateddate = '';
         } else {
             var localdate = new Date(dateField);
             // formatDate does not handle hour/minute
-            formateddate=$.datepicker.formatDate('yy/mm/dd', localdate) + "<br />" + localdate.getHours() + ":" + (localdate.getMinutes() < 10?"0":"") + localdate.getMinutes();
+            formateddate = $.datepicker.formatDate('yy/mm/dd', localdate) + '<br />' + localdate.getHours() + ':' + (localdate.getMinutes() < 10 ? '0' : '') + localdate.getMinutes();
         }
-        if ( $(this).prop('tagName') == 'TD' ) {
+        if ($(this).prop('tagName') == 'TD') {
             $(this).empty().append(formateddate);
         } else {
             //this is not a table row: prepend 'Date: '
@@ -69,47 +69,46 @@ function toLocalDate() {
 };
 
 function escapeExpression(str) {
-    return str.replace(/([#;&,_\-\.\+\*\~':"\!\^$\[\]\(\)=>\|])/g, "\\$1");
+    return str.replace(/([#;&,_\-\.\+\*\~':"\!\^$\[\]\(\)=>\|])/g, '\\$1');
 }
 
 function submittedReleaseButtons(buttonId) {
-    var btnId = '#' + escapeExpression( buttonId );
-    var other_btnId = btnId.replace("ready", "delete");
-    if ( other_btnId == btnId ) {
-        other_btnId = btnId.replace("delete", "ready");
+    var btnId = '#' + escapeExpression(buttonId);
+    var other_btnId = btnId.replace('ready', 'delete');
+    if (other_btnId == btnId) {
+        other_btnId = btnId.replace('delete', 'ready');
     }
-    if ( $( btnId ).is(':checked') ) {
-        $( other_btnId ).attr('checked', false);
-        $( other_btnId ).attr('disabled', true);
-    }
-    else {
-        $( other_btnId ).attr('disabled', false);
+    if ($(btnId).is(':checked')) {
+        $(other_btnId).attr('checked', false);
+        $(other_btnId).attr('disabled', true);
+    } else {
+        $(other_btnId).attr('disabled', false);
     }
 }
 
 function updateShip(e, shipped) {
-  csrf_token = $('#csrf_token').val();
-  if (shipped) {
-      status = "postrelease";
-      var d = new Date();
-      // Expects '%Y-%m-%d %H:%M:%S'
-      shippedAt = d.getFullYear() + "-" + (d.getMonth()+1) + "-" + d.getDate()+ " " + d.getHours() + ":" + d.getMinutes() + ":" + d.getSeconds();
-  } else {
-      status = "Started";
-      shippedAt = "";
-  }
-  var request = $.ajax( {
-    url: "/releases/" + e,
-    type: "POST",
-    data: "status=" + status + "&shippedAt=" + shippedAt + "&csrf_token=" + csrf_token,
-  });
+    csrf_token = $('#csrf_token').val();
+    if (shipped) {
+        status = 'postrelease';
+        var d = new Date();
+        // Expects '%Y-%m-%d %H:%M:%S'
+        shippedAt = d.getFullYear() + '-' + (d.getMonth() + 1) + '-' + d.getDate() + ' ' + d.getHours() + ':' + d.getMinutes() + ':' + d.getSeconds();
+    } else {
+        status = 'Started';
+        shippedAt = '';
+    }
+    var request = $.ajax({
+        url: '/releases/' + e,
+        type: 'POST',
+        data: 'status=' + status + '&shippedAt=' + shippedAt + '&csrf_token=' + csrf_token,
+    });
 
-  request.done(function() {
-     location.reload();
-  });
+    request.done(function() {
+        location.reload();
+    });
 
-  request.fail(function(jqXHR, textStatus) {
-     alert( "Error: " + jqXHR.responseText);
-  });
+    request.fail(function(jqXHR, textStatus) {
+        alert('Error: ' + jqXHR.responseText);
+    });
 
 }
