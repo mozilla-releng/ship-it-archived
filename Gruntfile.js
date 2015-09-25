6bb17595846a0ed4b87541e8f9fa361e8cc4bc63
Sylvestre Ledru <sylvestre@debian.org>
2015-09-25 12:34:06 +0200

    Add a JS linter with grunt integration

--- Gruntfile.js
+++ Gruntfile.js
@@ -2,8 +2,19 @@ module.exports = function(grunt) {
     grunt.initConfig({
         qunit: {
             files: ['kickoff/test/js/jstest.html']
+        },
+        jscs: {
+            main: "ship-it.js",
+            controllers: {
+                src: ["kickoff/static/kickoff.js", "kickoff/static/suggestions.js"],
+                options: {
+                    config: '.jscsrc',
+                    verbose: true
+                }
+            }
         }
     });
     grunt.loadNpmTasks('grunt-contrib-qunit');
-    grunt.registerTask('default', ['qunit']);
+    grunt.loadNpmTasks('grunt-jscs');
+    grunt.registerTask('default', ['qunit', 'jscs']);
 };
