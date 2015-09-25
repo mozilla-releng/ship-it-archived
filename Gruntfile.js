6644e5ad4682f13cc2115715ef395c111826e44c
Sylvestre Ledru <sylvestre@debian.org>
2015-09-25 14:42:43 +0200

    Also execute eslint as part of the automation

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,3 +1,4 @@
+sources = ["kickoff/static/kickoff.js", "kickoff/static/suggestions.js"];
 module.exports = function(grunt) {
     grunt.initConfig({
         qunit: {
@@ -6,15 +7,19 @@ module.exports = function(grunt) {
         jscs: {
             main: "ship-it.js",
             controllers: {
-                src: ["kickoff/static/kickoff.js", "kickoff/static/suggestions.js"],
+                src: sources,
                 options: {
                     config: '.jscsrc',
                     verbose: true
                 }
             }
+        },
+        eslint: {
+            target: sources
         }
     });
     grunt.loadNpmTasks('grunt-contrib-qunit');
     grunt.loadNpmTasks('grunt-jscs');
-    grunt.registerTask('default', ['qunit', 'jscs']);
+    grunt.loadNpmTasks('grunt-eslint');
+    grunt.registerTask('default', ['qunit', 'jscs', 'eslint']);
 };
