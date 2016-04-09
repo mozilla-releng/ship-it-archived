5d017181f129e5ae7b3ef7210e5b0f6d713cf156
ex-dev <allan.tavares@allantavares.com.br>
2016-04-09 17:39:07 -0300

    Added new page to edit a release and allow edit a not shipped release

--- kickoff/test/views/test_editRelease.py
+++ kickoff/test/views/test_editRelease.py
@@ -9,16 +9,19 @@ class TestEditRelease(ViewTest):
 		ret = self.get('/release/Firefox-2.0-build1/edit_release.html')
 		self.assertEquals(ret.status_code, 200)
 
+
 	def testGetReleaseWithError(self):
-		ret = self.get('/release/Firefox-1/edit_release.html')
+		ret = self.get('/release/Firefox-1-notfound/edit_release.html')
 		self.assertEquals(ret.status_code, 404)
+
 
 	def testEditRelease(self):
 		release = {
 			'shippedAtDate': '2013/03/03',
 			'shippedAtTime': '14:44:00',
 			'isSecurityDriven': True,
-			'description': 'Edited Release!'
+			'description': 'Edited Release!',
+			'isShipped': 'y'
 		}
 
 		ret = self.post('/release/Firefox-2.0-build1/edit_release.html', data=release)
@@ -34,6 +37,21 @@ class TestEditRelease(ViewTest):
 			dateAndTime = datetime.combine(dt, tm)
 			self.assertEquals(dbRelease._shippedAt, dateAndTime)
 
+
+	def testEditReleaseNotShipped(self):
+		release = {
+			'isSecurityDriven': True,
+			'description': 'Edited Release!'
+		}
+
+		ret = self.post('/release/Firefox-2.0-build1/edit_release.html', data=release)
+
+		with app.test_request_context():
+			dbRelease = FirefoxRelease.query.filter_by(name='Firefox-2.0-build1').first()
+			self.assertEquals(dbRelease.description, 'Edited Release!')
+			self.assertTrue(dbRelease.isSecurityDriven)
+
+
 	def testEditReleaseWithError(self):
 		release = {
 			'shippedAtDate': '2013/99/99',
@@ -44,3 +62,15 @@ class TestEditRelease(ViewTest):
 
 		ret = self.post('/release/Firefox-2.0-build1/edit_release.html', data=release)
 		self.assertEquals(ret.status_code, 400)
+
+
+	def testEditReleaseNotFount(self):
+		release = {
+			'shippedAtDate': '2013/99/99',
+			'shippedAtTime': '99:44:00',
+			'isSecurityDriven': True,
+			'description': 'Edited Release!'
+		}
+
+		ret = self.post('/release/Firefox-2.0-notfound/edit_release.html', data=release)
+		self.assertEquals(ret.status_code, 404)
