e523686a9df841a895d8dd6610afcb65d5fcacb2
Sylvestre Ledru <sylvestre@debian.org>
2016-06-06 15:07:51 +0200

    Merge day, update the version numbers

--- kickoff/config.py
+++ kickoff/config.py
@@ -1,10 +1,10 @@
 # Not used
-NIGHTLY_VERSION = "49.0a2"
-AURORA_VERSION = "48.0a2"
+NIGHTLY_VERSION = "50.0a2"
+AURORA_VERSION = "49.0a2"
 SUPPORTED_AURORA_LOCALES = ['ach', 'af', 'an', 'ar', 'as', 'ast', 'az', 'be', 'bg', 'bn-BD', 'bn-IN', 'br', 'brx', 'bs', 'ca', 'cs', 'cy', 'da', 'de', 'dsb', 'el', 'en-GB', 'en-US', 'en-ZA', 'eo', 'es-AR', 'es-CL', 'es-ES', 'es-MX', 'et', 'eu', 'fa', 'ff', 'fi', 'fr', 'fy-NL', 'ga-IE', 'gd', 'gl', 'gu-IN', 'he', 'hi-IN', 'hr', 'hsb', 'hu', 'hy-AM', 'id', 'is', 'it', 'ja', 'ka', 'kk', 'km', 'kn', 'ko', 'kok', 'ks', 'lij', 'lt', 'lv', 'mai', 'mk', 'ml', 'mr', 'ms', 'my', 'nb-NO', 'nl', 'nn-NO', 'oc', 'or', 'pa-IN', 'pl', 'pt-BR', 'pt-PT', 'rm', 'ro', 'ru', 'sat', 'si', 'sk', 'sl', 'sq', 'son', 'sr', 'sv-SE', 'ta', 'te', 'th', 'tr', 'uk', 'uz', 'vi', 'xh', 'zh-CN', 'zh-TW']
 LATEST_FIREFOX_OLDER_VERSION = "3.6.28"
-CURRENT_ESR = "38"
-ESR_NEXT = "45"
+CURRENT_ESR = "45"
+ESR_NEXT = ""
 JSON_FORMAT_VERSION = 1
 IOS_BETA_VERSION = "1.2"
 IOS_VERSION = "1.1"
