93e206adc371b4ffdc4a37b55851a9ff3a159abb
Nick Thomas <nrthomas@gmail.com>
2016-07-21 16:17:00 +1200

    Initialize the db at version 17, insert data, then upgrade to the latest

--- scripts/initdb_and_run.sh
+++ scripts/initdb_and_run.sh
@@ -3,8 +3,11 @@ if [ ! -e /app/.cache/mysql/db.done ]; then
     sleep 30
     echo "Initializing DB..."
     PYTHONPATH=${PYTHONPATH}:/app/vendor/lib/python python /app/migrate_repo/manage.py version_control mysql://shipit:shipitpw@shipitdb/shipit migrate_repo
-    PYTHONPATH=${PYTHONPATH}:/app/vendor/lib/python python /app/migrate_repo/manage.py upgrade mysql://shipit:shipitpw@shipitdb/shipit migrate_repo
+    # the sample data is for schema version 17
+    PYTHONPATH=${PYTHONPATH}:/app/vendor/lib/python python /app/migrate_repo/manage.py upgrade --version=17 mysql://shipit:shipitpw@shipitdb/shipit migrate_repo
     bunzip2 -c /app/scripts/sample-data.sql.bz2 | mysql -h shipitdb -u shipit --password=shipitpw shipit
+    # update to the latest schema
+    PYTHONPATH=${PYTHONPATH}:/app/vendor/lib/python python /app/migrate_repo/manage.py upgrade mysql://shipit:shipitpw@shipitdb/shipit migrate_repo
     touch /app/.cache/mysql/db.done
     echo "Done"
 else
