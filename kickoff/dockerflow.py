6090eee85d500df88251928be6880ef5d915f4bd
Johan Lorenzo <jlorenzo@mozilla.com>
2016-08-12 15:34:00 +0200

    Add DB check in /__heartbeat__

--- kickoff/dockerflow.py
+++ kickoff/dockerflow.py
@@ -1,18 +1,22 @@
 from os import path
 
 from flask import Response, jsonify
-
-# from auslib.global_state import dbo
+# Can't concatenate the next line with the following one. exc is not defined
+from flask.ext import sqlalchemy
+from sqlalchemy.exc import OperationalError
+from kickoff import db
 
 
 # Wrapper that creates the endpoints required by CloudOps' Dockerflow spec: https://github.com/mozilla-services/Dockerflow
 # This gets used by both the admin and public apps.
 def create_dockerflow_endpoints(app):
+    HEADERS = {"Cache-Control": "no-cache"}
+
     @app.route("/__version__")
     def version():
         version_file = app.config.get("VERSION_FILE")
         if version_file and path.exists(version_file):
-            with open(app.config["VERSION_FILE"]) as f:
+            with open(version_file) as f:
                 version_json = f.read()
             return Response(version_json, mimetype="application/json", headers={"Cache-Control": "no-cache"})
         else:
@@ -27,14 +31,15 @@ def create_dockerflow_endpoints(app):
         """Per the Dockerflow spec:
         Respond to /__heartbeat__ with a HTTP 200 or 5xx on error. This should
         depend on services like the database to also ensure they are healthy."""
-        # Counting the rules should be a trivial enough operation that it won't
-        # cause notable load, but will verify that the database works.
-        # dbo.rules.countRules()
-        return Response("OK!", headers={"Cache-Control": "no-cache"})
+        try:
+            db.engine.execute('SELECT 1').fetchall()
+            return Response("OK!", headers=HEADERS)
+        except OperationalError:
+            return Response("Not OK", status=500, headers=HEADERS)
 
     @app.route("/__lbheartbeat__")
     def lbheartbeat():
         """Per the Dockerflow spec:
         Respond to /__lbheartbeat__ with an HTTP 200. This is for load balancer
         checks and should not check any dependent services."""
-        return Response("OK!", headers={"Cache-Control": "no-cache"})
+        return Response("OK!", headers=HEADERS)
