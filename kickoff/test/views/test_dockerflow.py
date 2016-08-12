6794e4309844ffe38cef33d69d021ed6339e396d
Johan Lorenzo <jlorenzo@mozilla.com>
2016-08-12 12:06:44 +0200

    Add test for __version__

---
+++ kickoff/test/views/test_dockerflow.py
@@ -0,0 +1,37 @@
+import os
+from tempfile import mkstemp
+
+from kickoff import app
+from kickoff.test.views.base import ViewTest
+
+
+class TestSubmitRelease(ViewTest):
+
+    def setUp(self):
+        ViewTest.setUp(self)
+        self.version_fd, self.version_file = mkstemp()
+        app.config["VERSION_FILE"] = self.version_file
+
+        with open(self.version_file, "w+") as f:
+            f.write("""
+{
+  "source":"https://github.com/mozilla/balrog",
+  "version":"1.0",
+  "commit":"abcdef123456"
+}
+""")
+
+    def tearDown(self):
+        os.close(self.version_fd)
+        os.remove(self.version_file)
+        ViewTest.tearDown(self)
+
+    def testVersion(self):
+        ret = self.get("/__version__")
+        self.assertEqual(ret.data, """
+{
+  "source":"https://github.com/mozilla/balrog",
+  "version":"1.0",
+  "commit":"abcdef123456"
+}
+""")
