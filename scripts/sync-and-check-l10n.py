b14371bb6347cd290fd1fefe1f821e01d5469c16
Francesco Lodolo (:flod) <flod@lodolo.net>
2016-09-08 14:46:20 +0000

    Fix indentation in sync l10n script

--- scripts/sync-and-check-l10n.py
+++ scripts/sync-and-check-l10n.py
@@ -12,29 +12,29 @@ from config import SUPPORTED_NIGHTLY_LOCALES
 
 
 def readConfFile(url):
-        print("Synchronizing from " + url)
-        response = urllib2.urlopen(url)
-        content = response.read()
-        ini_str = '[root]\n' + content
-        ini_fp = StringIO.StringIO(ini_str)
-        config = ConfigParser.RawConfigParser()
-        config.readfp(ini_fp)
-        return config.items('root')
+    print("Synchronizing from " + url)
+    response = urllib2.urlopen(url)
+    content = response.read()
+    ini_str = '[root]\n' + content
+    ini_fp = StringIO.StringIO(ini_str)
+    config = ConfigParser.RawConfigParser()
+    config.readfp(ini_fp)
+    return config.items('root')
 
 
 def saveJSON(url, country):
-        countries = readConfFile(url)
-        valuesOrdered = OrderedDict(sorted(dict(countries).items(), key=lambda x: x[1]))
-        dump = json.dumps(valuesOrdered, indent=4, separators=(',', ': '))
+    countries = readConfFile(url)
+    valuesOrdered = OrderedDict(sorted(dict(countries).items(), key=lambda x: x[1]))
+    dump = json.dumps(valuesOrdered, indent=4, separators=(',', ': '))
 
-        # Save
-        with open(pathToLib + "/static/regions/%s.json" % country, "w") as text_file:
-                text_file.write(dump)
+    # Save
+    with open(pathToLib + "/static/regions/%s.json" % country, "w") as text_file:
+        text_file.write(dump)
 
 
 def getListLocalesFromURL(URL):
-        response = urllib2.urlopen(URL)
-        return response.read().splitlines()
+    response = urllib2.urlopen(URL)
+    return response.read().splitlines()
 
 enUSURL = "https://hg.mozilla.org/releases/mozilla-aurora/raw-file/tip/toolkit/locales/en-US/chrome/global/regionNames.properties"
 saveJSON(enUSURL, "en-US")
@@ -44,27 +44,27 @@ listLocaleURL = "https://raw.githubusercontent.com/mozilla-l10n/mozilla-l10n-que
 remoteAuroraLocalesList = getListLocalesFromURL(listLocaleURL)
 warnings = []
 for loc in SUPPORTED_AURORA_LOCALES:
-        if loc != 'en-US':
-                try:
-                        url = "https://hg.mozilla.org/releases/l10n/mozilla-aurora/%s/raw-file/tip/toolkit/chrome/global/regionNames.properties" % loc
-                        saveJSON(url, loc)
-                        if loc in remoteAuroraLocalesList:
-                                remoteAuroraLocalesList.remove(loc)
-                except:
-                        warnings.append("Warning: regionNames.properties is not available for %s" % loc)
+    if loc != 'en-US':
+        try:
+            url = "https://hg.mozilla.org/releases/l10n/mozilla-aurora/%s/raw-file/tip/toolkit/chrome/global/regionNames.properties" % loc
+            saveJSON(url, loc)
+            if loc in remoteAuroraLocalesList:
+                remoteAuroraLocalesList.remove(loc)
+        except:
+            warnings.append("Warning: regionNames.properties is not available for %s" % loc)
 if remoteAuroraLocalesList:
-        for loc in remoteAuroraLocalesList:
-                warnings.append("Warning: '%s' NOT found in SUPPORTED_AURORA_LOCALES" % loc)
+    for loc in remoteAuroraLocalesList:
+        warnings.append("Warning: '%s' NOT found in SUPPORTED_AURORA_LOCALES" % loc)
 if warnings:
-        print ("\n".join(warnings))
-        print("Sanity check: %d locales not in SUPPORTED_AURORA_LOCALES (lo, tsz and wo are expected)" % len(remoteAuroraLocalesList))
+    print ("\n".join(warnings))
+    print("Sanity check: %d locales not in SUPPORTED_AURORA_LOCALES (lo, tsz and wo are expected)" % len(remoteAuroraLocalesList))
 
 nbLocaleMissing = 0
 listLocaleNightlyURL = "https://raw.githubusercontent.com/mozilla-l10n/mozilla-l10n-query/master/app/sources/central.txt"
 for loc in getListLocalesFromURL(listLocaleNightlyURL):
-        if loc not in SUPPORTED_NIGHTLY_LOCALES:
-                nbLocaleMissing = nbLocaleMissing + 1
-                print("Warning: '%s' NOT found in SUPPORTED_NIGHTLY_LOCALES" % loc)
+    if loc not in SUPPORTED_NIGHTLY_LOCALES:
+        nbLocaleMissing = nbLocaleMissing + 1
+        print("Warning: '%s' NOT found in SUPPORTED_NIGHTLY_LOCALES" % loc)
 print("Sanity check: %d locales not in SUPPORTED_NIGHTLY_LOCALES" % nbLocaleMissing)
 
 print("Note that ship-it IS the source of truth for supported locales")
