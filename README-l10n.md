ccbec96fc16b3d8ecd5f85930ae062798cd01f28
Francesco Lodolo <flod@lodolo.net>
2016-09-08 07:46:05 +0000

    Bug 1301026 - Improve l10n sync script, add documentationBug 1301026: use  (#122)
    * Bug 1301026 - Use SUPPORTED_AURORA_LOCALES to scrap regionNames.properties
    
    * Add README for l10n

---
+++ README-l10n.md
@@ -0,0 +1,8 @@
+# Process for l10n
+## Adding a new locale to Aurora builds
+
+1. Add the new locale code to `SUPPORTED_AURORA_LOCALES` in `kickoff/config.py`.
+
+2. Make sure that the language name (both English and localized) is available in `kickoff/static/languages.json`.
+
+3. Run `scripts/scripts/sync-and-check-l10n.py` to download `regionNames.properties` for this new locale.
--- scripts/sync-and-check-l10n.py
+++ scripts/sync-and-check-l10n.py
@@ -41,14 +41,23 @@ saveJSON(enUSURL, "en-US")
 
 
 listLocaleURL = "https://raw.githubusercontent.com/mozilla-l10n/mozilla-l10n-query/master/app/sources/aurora.txt"
-nbLocaleMissing = 0
-for loc in getListLocalesFromURL(listLocaleURL):
-        url = "https://hg.mozilla.org/releases/l10n/mozilla-aurora/%s/raw-file/tip/toolkit/chrome/global/regionNames.properties" % loc
-        saveJSON(url, loc)
-        if loc not in SUPPORTED_AURORA_LOCALES:
-                nbLocaleMissing = nbLocaleMissing + 1
-                print("Warning: '%s' NOT found in SUPPORTED_AURORA_LOCALES" % loc)
-print("Sanity check: %d locales not in SUPPORTED_AURORA_LOCALES (lo, tsz and wo are expected)" % nbLocaleMissing)
+remoteAuroraLocalesList = getListLocalesFromURL(listLocaleURL)
+warnings = []
+for loc in SUPPORTED_AURORA_LOCALES:
+        if loc != 'en-US':
+                try:
+                        url = "https://hg.mozilla.org/releases/l10n/mozilla-aurora/%s/raw-file/tip/toolkit/chrome/global/regionNames.properties" % loc
+                        saveJSON(url, loc)
+                        if loc in remoteAuroraLocalesList:
+                                remoteAuroraLocalesList.remove(loc)
+                except:
+                        warnings.append("Warning: regionNames.properties is not available for %s" % loc)
+if remoteAuroraLocalesList:
+        for loc in remoteAuroraLocalesList:
+                warnings.append("Warning: '%s' NOT found in SUPPORTED_AURORA_LOCALES" % loc)
+if warnings:
+        print ("\n".join(warnings))
+        print("Sanity check: %d locales not in SUPPORTED_AURORA_LOCALES (lo, tsz and wo are expected)" % len(remoteAuroraLocalesList))
 
 nbLocaleMissing = 0
 listLocaleNightlyURL = "https://raw.githubusercontent.com/mozilla-l10n/mozilla-l10n-query/master/app/sources/central.txt"
@@ -59,4 +68,4 @@ for loc in getListLocalesFromURL(listLocaleNightlyURL):
 print("Sanity check: %d locales not in SUPPORTED_NIGHTLY_LOCALES" % nbLocaleMissing)
 
 print("Note that ship-it IS the source of truth for supported locales")
-print("Please report a bug to have a locale added/remove: https://bugzilla.mozilla.org/enter_bug.cgi?product=www.mozilla.org&component=Product%20Details")
+print("Please report a bug to have a locale added/remove: https://bugzilla.mozilla.org/enter_bug.cgi?product=Release%20Engineering&component=Ship%20It")
