66749b3603842583a6317a4a62b07e114e85f758
Kohei Yoshino <kohei.yoshino@gmail.com>
2016-09-09 19:51:31 -0400

    Add Fennec Aurora/Nightly supported locales, part of Bug 1301650

--- kickoff/config.py
+++ kickoff/config.py
@@ -12,6 +12,12 @@ JSON_FORMAT_L10N_VERSION = "1.0"
 IOS_BETA_VERSION = "6.0"
 IOS_VERSION = "5.0"
 
+# Firefox for Android supported locales
+ANDROID_NIGHTLY_LOCALES = ["ar", "be", "ca", "cs", "da", "de", "es-AR", "es-ES", "et", "eu", "fa", "fi", "fr", "fy-NL", "ga-IE", "gd", "gl", "he", "hu", "id", "it", "ja", "ko", "lt", "nb-NO", "nl", "nn-NO", "pa-IN", "pl", "pt-BR", "pt-PT", "ro", "ru", "sk", "sl", "sq", "sr", "sv-SE", "th", "tr", "uk", "zh-CN", "zh-TW"]
+ANDROID_ALPHA_LOCALES = ["an", "ar", "as", "ast", "az", "be", "bn-BD", "bn-IN", "br", "ca", "cak", "cs", "cy", "da", "de", "dsb", "el", "en-GB", "en-ZA", "eo", "es-AR", "es-CL", "es-ES", "es-MX", "et", "eu", "fa", "ff", "fi", "fr", "fy-NL", "ga-IE", "gd", "gl", "gn", "gu-IN", "he", "hi-IN", "hr", "hsb", "hu", "hy-AM", "id", "is", "it", "ja", "ka", "kk", "kn", "ko", "lo", "lt", "lv", "mai", "ml", "mr", "ms", "my", "nb-NO", "nl", "nn-NO", "or", "pa-IN", "pl", "pt-BR", "pt-PT", "rm", "ro", "ru", "sk", "sl", "son", "sq", "sr", "sv-SE", "ta", "te", "th", "tr", "tsz", "uk", "ur", "uz", "wo", "xh", "zh-CN", "zh-TW"]
+ANDROID_BETA_LOCALES = ["cs", "de", "en-US", "es-ES", "fi", "fr", "it", "ja", "nl", "pl", "pt-PT", "ru"]
+ANDROID_RELEASE_LOCALES = ["ar", "be", "ca", "cs", "de", "en-US", "es-AR", "es-ES", "eu", "fa", "fi", "fr", "fy-NL", "ga-IE", "gl", "hu", "it", "ja", "lt", "nl", "pa-IN", "pl", "pt-BR", "pt-PT", "ro", "ru", "sk", "tr", "uk", "zh-CN", "zh-TW"]
+
 # The keyword used in the ship-it db for old release before the changeset/json l10n files
 LEGACY_KEYWORD = "legacy"
 BETA_AGGREGATION_KEYWORD = "beta"
