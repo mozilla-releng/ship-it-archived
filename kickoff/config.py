9a6f22491fda54992ea0994fa31a0b49efc23877
Pascal Chevrel <pascal@mozilla.com>
2017-09-21 18:37:23 +0200

    Nightly 58 (#191)

--- kickoff/config.py
+++ kickoff/config.py
@@ -1,5 +1,5 @@
 # To make flake8 happy
-NIGHTLY_VERSION = "57.0a1"
+NIGHTLY_VERSION = "58.0a1"
 LATEST_THUNDERBIRD_NIGHTLY_VERSION = "57.0a1"
 LATEST_THUNDERBIRD_ALPHA_VERSION = "54.0a2"
 SUPPORTED_NIGHTLY_LOCALES = ['ach', 'af', 'an', 'ar', 'as', 'ast', 'az', 'be', 'bg', 'bn-BD', 'bn-IN', 'br', 'bs', 'ca', 'cak', 'cs', 'cy', 'da', 'de', 'dsb', 'el', 'en-GB', 'en-US', 'en-ZA', 'eo', 'es-AR', 'es-CL', 'es-ES', 'es-MX', 'et', 'eu', 'fa', 'ff', 'fi', 'fr', 'fy-NL', 'ga-IE', 'gd', 'gl', 'gn', 'gu-IN', 'he', 'hi-IN', 'hr', 'hsb', 'hu', 'hy-AM', 'id', 'is', 'it', 'ja', 'ja-JP-mac', 'ka', 'kab', 'kk', 'km', 'kn', 'ko', 'lij', 'lo', 'lt', 'ltg', 'lv', 'mai', 'mk', 'ml', 'mr', 'ms', 'my', 'nb-NO', 'ne-NP', 'nl', 'nn-NO', 'or', 'pa-IN', 'pl', 'pt-BR', 'pt-PT', 'rm', 'ro', 'ru', 'si', 'sk', 'sl', 'sq', 'son', 'sr', 'sv-SE', 'ta', 'te', 'th', 'tl', 'tr', 'uk', 'ur', 'uz', 'vi', 'xh', 'zh-CN', 'zh-TW']
