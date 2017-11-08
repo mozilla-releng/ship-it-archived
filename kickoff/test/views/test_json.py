9a8bc3106b60dc65e2752da16ebe965c7ee8b5c6
Nick Thomas <nrthomas@gmail.com>
2017-11-08 16:43:35 +1300

    Add a test for the case where deved and beta are both on a version

--- kickoff/test/views/test_json.py
+++ kickoff/test/views/test_json.py
@@ -1,8 +1,10 @@
 import re
 import simplejson as json
+from datetime import datetime
 
-from kickoff import config
+from kickoff import config, app, db
 from kickoff.test.views.base import ViewTest
+from kickoff.model import FirefoxRelease
 
 JSON_VER = config.JSON_FORMAT_VERSION
 BASE_JSON_PATH = '/json/' + JSON_VER
@@ -115,6 +117,32 @@ class TestJSONRequestsAPI(ViewTest):
         self.assertEquals(len(primary['en-US']), 7)
         # ja-JP-mac is not a locale we want to expose into product-details
         self.assertFalse('ja-JP-mac' in primary)
+
+    def testPrimaryBuildsDegenerateDevedAndBeta(self):
+        # add a beta 3.0b5 to match deved
+        with app.test_request_context():
+            r = FirefoxRelease(partials='0,1', promptWaitTime=5,
+                               submitter='joe', version='3.0b5', buildNumber=1,
+                               branch='a', mozillaRevision='defau',
+                               l10nChangesets='ja zu',
+                               mozillaRelbranch='FOO',
+                               submittedAt=datetime(2005, 2, 3, 4, 5, 6, 7),
+                               shippedAt=datetime(2005, 2, 3, 4, 5, 6, 7),
+                               comment="yet an other amazing comment",
+                               isSecurityDriven=True,
+                               description="Another beta release for Firefox 3")
+            r.complete = True
+            r.ready = True
+            r.status = "shipped"
+            db.session.add(r)
+
+            db.session.commit()
+
+        ret = self.get(BASE_JSON_PATH + '/firefox_primary_builds.json')
+        self.assertEquals(ret.status_code, 200)
+        primary = json.loads(ret.data)
+        self.assertFalse(any(['3.0b3' in primary[l] for l in primary]))  # older beta not present
+        self.assertTrue('3.0b5' in primary['en-US'])
 
     def testPrimaryBuildsDontShowEsrNextIfNonePresent(self):
         config.ESR_NEXT = ''
