36665384acc6a37e6bc838c38a2a6a4a93aca73c
Nick Thomas <nthomas-mozilla@users.noreply.github.com>
2017-11-08 15:37:53 +1300

    Update sample data to latest from production. (#201)
    NB:
    * Devedition 58.0b1 in shipped state but no equivalent beta yet
    * adds mysql-client to package list so the sample data can be inserted

--- scripts/initdb_and_run.sh
+++ scripts/initdb_and_run.sh
@@ -6,8 +6,8 @@ if [ ! -e /app/.cache/mysql/db.done ]; then
     sleep 30
     echo "Initializing DB..."
     PYTHONPATH=${PYTHONPATH}:/app/vendor/lib/python python /app/migrate_repo/manage.py version_control mysql://shipit:shipitpw@shipitdb/shipit migrate_repo
-    # the sample data is for schema version 19
-    PYTHONPATH=${PYTHONPATH}:/app/vendor/lib/python python /app/migrate_repo/manage.py upgrade --version=19 mysql://shipit:shipitpw@shipitdb/shipit migrate_repo
+    # the sample data is for schema version 21
+    PYTHONPATH=${PYTHONPATH}:/app/vendor/lib/python python /app/migrate_repo/manage.py upgrade --version=21 mysql://shipit:shipitpw@shipitdb/shipit migrate_repo
     bunzip2 -c /app/scripts/sample-data.sql.bz2 | mysql -h shipitdb -u shipit --password=shipitpw shipit
     # update to the latest schema
     PYTHONPATH=${PYTHONPATH}:/app/vendor/lib/python python /app/migrate_repo/manage.py upgrade mysql://shipit:shipitpw@shipitdb/shipit migrate_repo
